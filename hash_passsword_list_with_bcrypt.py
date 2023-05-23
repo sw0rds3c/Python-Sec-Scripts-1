cabab351a3834d2030b5586a018a2d6127b64e6a
sw0rds3c <99446293+sw0rds3c@users.noreply.github.com>
2023-05-23 10:08:20 -0700

    Update hash_passsword_list_with_bcrypt.py

--- hash_passsword_list_with_bcrypt.py
+++ hash_passsword_list_with_bcrypt.py
@@ -1,41 +1,14 @@
-'''
-Script:  Final Scripting Project
-Author:  Mike Sword
-Date:    March 2021
-Version: .007
-Purpose: 
-
-
-         1st Part --> Generates a set number of files that get filled with randomized, multi-character, set-length passwords up to a set number of passwords per file.
-
-         2nd Part --> Hashes the passwords in the file(s) --and any other .txt-- for more secure storage using bycrypt. Bcrypt takes a set number of salt rounds and prepends it to the hash of the password.
-                  --> The user is prompted for a filepath where the password list(s) they would want to be salted & hashed are held.
-                  --> The path is then walked and any files that are .txt are pulled for conversion; Processing .csv and .json can easily get implemented in.
-                  --> The .txt file gets processed and blank lines and blank spaces are removed. Then the new list is hashed, password by password, and appends them to a single file. Multi-file output can be implemented.
-                  
-                  
-Why this is useful:
-                  --> This script is useful for securing stored passwords that are from something like a web-app authentication database for instance.
-                  --> It's always a terrible idea to have passwords stored as plaintext..
-                  
-Note:
-                  --> To make hashes more secure, set rounds of salt >= 12.
-                  
-                      
-'''
-
-
 import os   
 import sys
 import random
 import string
 import bcrypt   # pip install bcrypt
 
-
+#PART1
 # Creates a random passsword (string) of lower and uppercase letters, numbers, and special characters.
 # Then adds this string to a text file.
 # When the set number of passwords per file is met, open another file until the set file number count is met.
-
+'''
 numFiles = 4
 numPasswords = 250
 passwordLength = 12
@@ -68,18 +41,12 @@ try:
         
 except Exception as err:
     print (str(err))
-    
-    
+'''
 
-
-    
-    
-    
+#PART2
 # Now we prompt the user for input to get the file path for the password list(s) to be hashed.
 # For each password file, we want to pull all the entries from each file and hash them with bcrypt.
 # Then we want to store those hashed entries in another text file for secure storage.
-
-## c:\users\administrator\desktop\cybv312 sp21\final ##
 
 DIR = input("\nEnter File Path for password list processing -- (Text Files Only!): ")
 
